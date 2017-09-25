1af55d61327066087b45794fedd882a17a1cf30f
Aydan Rende <aydan.rende@dfki.de>
2017-09-25 10:59:29 +0200

    Aliases of items are added as independent docs but using the same id.
    This would help to match different mentions with one entity.

--- src/main/java/de/dfki/mlt/kbe/EntitySink.java
+++ src/main/java/de/dfki/mlt/kbe/EntitySink.java
@@ -4,6 +4,8 @@
 package de.dfki.mlt.kbe;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.flink.api.common.functions.RuntimeContext;
 import org.apache.flink.streaming.connectors.elasticsearch2.ElasticsearchSinkFunction;
@@ -12,6 +14,7 @@ import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.Requests;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -22,36 +25,59 @@ import org.json.JSONObject;
 public class EntitySink implements ElasticsearchSinkFunction<JSONObject> {
 	private static final long serialVersionUID = 1L;
 
-	public IndexRequest createEntityIndexRequest(JSONObject jsonObj)
+	public List<IndexRequest> createEntityIndexRequest(JSONObject jsonObj)
 			throws IOException, JSONException {
+		List<IndexRequest> requestList = new ArrayList<IndexRequest>();
 		String type = jsonObj.getString("type");
 		String id = jsonObj.getString("id");
-		JSONObject labelObj = jsonObj
-				.getJSONObject("labels")
-				.getJSONObject("en");
+		JSONObject labelObj = jsonObj.getJSONObject("labels").getJSONObject(
+				"en");
 		String label = labelObj.getString("value");
 		XContentBuilder builder = XContentFactory.jsonBuilder().startObject()
-				.field("type", type).field("id", id).field("label", label)
+				.field("id", id).field("type", type).field("label", label)
 				.endObject();
 
-		String json = builder.string();
-		IndexRequest indexRequest = Requests.indexRequest()
-				.index("wikidata-index").type("wikidata-entity").source(json);
+		requestList.add(Requests.indexRequest().index("wikidata-index")
+				.type("wikidata-entity").source(builder.string()));
 
-		return indexRequest;
+		JSONArray aliasArr = jsonObj.getJSONObject("aliases").getJSONArray("en");
+
+		requestList.addAll(createEntityIndexRequestFromAliases(id, type,
+				aliasArr));
+		return requestList;
+	}
+
+	private List<IndexRequest> createEntityIndexRequestFromAliases(String id,
+			String type, JSONArray aliasArr) throws IOException {
+		List<IndexRequest> requestList = new ArrayList<IndexRequest>();
+		String label = "";
+		XContentBuilder builder = null;
+		for (Object aliasObj : aliasArr) {
+			label = ((JSONObject) aliasObj).getString("value");
+			builder = XContentFactory.jsonBuilder().startObject()
+					.field("id", id).field("type", type).field("label", label)
+					.endObject();
+
+			requestList.add(Requests.indexRequest().index("wikidata-index")
+					.type("wikidata-entity").source(builder.string()));
+		}
+		return requestList;
 	}
 
 	@Override
 	public void process(JSONObject element, RuntimeContext ctx,
 			RequestIndexer indexer) {
+		List<IndexRequest> requestList = new ArrayList<IndexRequest>();
 		try {
-			indexer.add(createEntityIndexRequest(element));
+			requestList = createEntityIndexRequest(element);
+			IndexRequest[] requestArray = requestList
+					.toArray(new IndexRequest[0]);
+			indexer.add(requestArray);
 		} catch (JSONException e) {
-			// e.printStackTrace();
+			 e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 }
